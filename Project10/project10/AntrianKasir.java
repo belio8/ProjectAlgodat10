878007f07a06aa3a0d56f1ed968c520ddded1425
inazputri <nzkmila@gmail.com>
2025-12-07 16:33:43 +0800

    queue

--- Project10/project10/AntrianKasir.java
+++ Project10/project10/AntrianKasir.java
@@ -1,7 +1,7 @@
 public class AntrianKasir {
     Pelanggan front, rear;
 
-    public void enqueue(String nama, KeranjangBelanja keranjang) {
+    public void tambahPelanggan(String nama, KeranjangBelanja keranjang) {
         Pelanggan p = new Pelanggan(nama, keranjang);
         if (rear == null) {
             front = rear = p;
@@ -12,7 +12,7 @@ public class AntrianKasir {
         System.out.println(">> " + nama + " berhasil masuk antrian kasir.");
     }
 
-    public Pelanggan dequeue() {
+    public Pelanggan hapusPelanggan() {
         if (front == null) return null;
         Pelanggan temp = front;
         front = front.next;
